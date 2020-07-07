f7a8fb4226a49433a99e5f7d193295472c052dcb
zohaibk4 <zohaibk1204@gmail.com>
2020-07-07 17:28:00 -0400

    Understanding Stack

--- Exercises/Chapter3/Stack/Stack.go
+++ Exercises/Chapter3/Stack/Stack.go
@@ -1,13 +1,30 @@
 package main
 
-func push(arr []int) []int {
+import "fmt"
 
+type stack struct {
+	i    int
+	data [10]int
 }
 
-func pop(arr []int) []int {
+func (s *stack) push(k int) {
+	s.data[s.i] = k
+	s.i++
+}
 
+func (s *stack) pop() int {
+	s.i--
+	ret := s.data[s.i]
+	s.data[s.i] = 0
+	return ret
 }
 
 func main() {
-
+	var s stack
+	s.push(12)
+	s.push(4)
+	s.push(9)
+	s.pop()
+	s.push(2002)
+	fmt.Printf("stack %v\n", s)
 }
